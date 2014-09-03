b3e823f6e7053643a8f501c24133211cd946358d
DmitryKosmich <kosmich27@mail.ru>
2014-09-03 13:11:51 +0300

    It removes artifacts of old project

--- public/js/logic/services/popup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function(){
-    $( ".reveal-modal-bg_country_popup").click( function(e) {
-        if (e.target.id === "bg_country_popup"){
-            countryPopUpHide();
-        }
-    });
-})();
